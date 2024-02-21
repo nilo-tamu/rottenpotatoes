b20b5897c42817dc4df6bdd700216bd19baceeec
Nilo Lisboa <nilolisboa@Nilos-MacBook-Pro.local>
2024-02-21 00:11:32 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
    :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-   :release_date => '13-Aug-2021'}
+   :release_date => '13-Aug-2021'},
+  {:title => 'Dragon Ball Super: Broly', :rating => 'PG',
+   :release_date => '16-Jan-2019'},
+  {:title => 'No Country for Old Men', :rating => 'R',
+   :release_date => '9-Nov-2007'},
+  {:title => 'The Menu', :rating => 'R',
+   :release_date => '18-Nov-2022'},
 ]
 
 more_movies.each do |movie|
